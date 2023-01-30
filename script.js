24b8f71615dd29cc8c191255d3a1402837c3ba05
emxd <emad.321a@gmail.com>
2023-01-30 18:30:26 +1100

    Option to change color implemented

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const canvasContainer = document.querySelector("#canvasContainer");
 const sizeBtn = document.querySelector(".sizeBtn");
 const clearBtn = document.querySelector(".clearGridBtn");
+const rainbowBtn = document.querySelector(".rainbowBtn");
+const gradientBtn = document.querySelector(".gradientBtn");
+const colorPicker = document.querySelector("#colorPicker");
+let solidColor = true;
+let rainbowColor = false;
+let gradientColor = false;
+
 
 let dynamicDivCreator = (dimensions) => {
   canvasContainer.innerHTML = "";
@@ -31,9 +38,22 @@ let boxPressed = (e) => {
     return;
   }
 
-  document.querySelector("#" + e.target.id).setAttribute("style", "background-color: red;");
+  if (solidColor){
+    document.querySelector("#" + e.target.id).setAttribute("style", "background-color: " + colorPicker.value +";");
+  } else if(rainbowColor)
+  {
+    let hashColor = rainbowColowGenerator();
+    document.querySelector("#" + e.target.id).setAttribute("style", "background-color: " + hashColor +";");
+  } else if(gradientColor){
+
+  }
   console.log(e.target.id);
 };
+
+let rainbowColowGenerator = () =>{
+    let color = "#" + Math.floor(Math.random()*16777215).toString(16);
+    return color;
+}
 
 canvasContainer.addEventListener("mouseover", boxPressed);
 sizeBtn.addEventListener("click", () => {
@@ -41,5 +61,17 @@ sizeBtn.addEventListener("click", () => {
   dynamicDivCreator(sizeOption);
 });
 clearBtn.addEventListener("click", clearGrid);
+
+rainbowBtn.addEventListener("click", () => {
+    solidColor = false, rainbowColor = true, gradientColor = false;
+})
+
+gradientBtn.addEventListener("click", () => {
+    solidColor = false, rainbowColor = false, gradientColor = true;
+})
+
+colorPicker.addEventListener("change", () => {
+    solidColor = true, rainbowColor = false, gradientColor = false;
+})
 
 dynamicDivCreator(8);
